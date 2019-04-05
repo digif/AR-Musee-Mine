164ad2a85dbdcabe4c0d204533ecb1bfe2669d55
digif <digifi@laposte.net>
2019-04-05 11:41:11 +0200

    working UI +3 new objects

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public void GoToMenu (GameObject targetMenu)
+    {
+        targetMenu.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+    public void GoToScene (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
