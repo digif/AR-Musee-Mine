3c907dc06a20415147bccec9ff8b8017c5f5780c
Burak <burak.kucukal@isen.yncrea.fr>
2019-04-17 16:49:28 +0200

    comments
    méthodes des boutons des lumières de la lampe et du casque.

--- Assets/Scripts/Bouton_casque_lumiere.cs
+++ Assets/Scripts/Bouton_casque_lumiere.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class Bouton_casque_lumiere : MonoBehaviour
 {
-    public GameObject lampe;
+    //Script qui permet d'allumer la lampe du casque.
+
+    public GameObject lampe; // Il s'agit de la lampe du casque
+
+    // On detecte l'appui sur le bouton à l'aide de la commande OnMouseDown et en fonction de l'état actuel de la lampe du casque,
+    // on l'allume ou on l'éteint en faisant varier son intensité. Nous avons également animé le bouton pour que l'utilisateur
+    // aie un feedback en cas d'appui. L'animation se gère avec le composant Animator du bouton.
+
     private void OnMouseDown() {
         if (lampe.GetComponent<Light>().intensity == 0){
             lampe.GetComponent<Light>().intensity = 10;
-            gameObject.GetComponent<Animator>().SetTrigger("trig_lum_casque");
+            gameObject.GetComponent<Animator>().SetTrigger("trig_lum_casque");//Pour lancer l'animation on trigger la variable préalablement créée trig_lum_casque.
         }
         else {
             lampe.GetComponent<Light>().intensity = 0;
             gameObject.GetComponent<Animator>().SetTrigger("trig_lum_casque");
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        //lampe = GameObject.Find("lumière_casque");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/Bouton_lampe.cs
+++ Assets/Scripts/Bouton_lampe.cs
@@ -4,27 +4,21 @@ using UnityEngine;
 
 public class Bouton_lampe : MonoBehaviour
 {
-    public GameObject lumiere;
+    //Script qui permet d'allumer la lampe du casque.
+    public GameObject lumiere;// Il s'agit de la lampe du casque
+
+    // On detecte l'appui sur le bouton à l'aide de la commande OnMouseDown et en fonction de l'état actuel de la lampe, on
+    // l'allume ou on l'éteint en faisant varier son intensité. Nous avons également animé le bouton pour que l'utilisateur
+    // aie un feedback en cas d'appui. L'animation se gère avec le composant Animator du bouton.
     private void OnMouseDown() {
         if (lumiere.GetComponent<Light>().intensity == 0){
             lumiere.GetComponent<Light>().intensity = 1000;
-            gameObject.GetComponent<Animator>().SetTrigger("appui_btn_lampe");
+            gameObject.GetComponent<Animator>().SetTrigger("appui_btn_lampe");//Pour lancer l'animation on trigger la variable préalablement créée appui_btn_lampe.
         }
         else {
             lumiere.GetComponent<Light>().intensity = 0;
             gameObject.GetComponent<Animator>().SetTrigger("appui_btn_lampe");
         }
-        
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 }
