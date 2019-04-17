a19dbfb0c9911afa0c4903129a404cc06e7d1c18
Burak <burak.kucukal@isen.yncrea.fr>
2019-04-17 16:28:38 +0200

    Description + quelques commentaires
    Ajout et modification des description de tous les objets + commentaires sur la méthode qui permet de choisir la description et le titre à afficher.

--- Assets/Scripts/UI/ObjectsConstants.cs
+++ Assets/Scripts/UI/ObjectsConstants.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class ObjectsConstants 
 {
-    public static readonly string[] GRISOUMETRE = { "Grisoumètre", "description du grisoumètre" };
-    public static readonly string[] GENEPHONE = {"Genephone", "description du genephone" };
-    public static readonly string[] CHAUSSURES = {"Chaussures", "description des chaussures" };
-    public static readonly string[] BRIQUET = { "Briquet","description du briquet"};
-    public static readonly string[] LAMPISTERIE = {"Lampisterie", "description de la lampisterie" };
+    public static readonly string[] GRISOUMETRE = { "Grisoumètre", "Le Grisoumètre est un appareil servant à mesurer le taux de grisou dans une mine. Il s'agit le plus souvent d'un instrument permettant de mesurer la teneur en grisou dans l'air ambiant, estimée principalement selon la hauteur d'une flamme." };
+    public static readonly string[] GENEPHONE = {"Genephone", "Le généphone fonctionne simplement avec deux fils pour relier les combinés. Il génère (d'où son nom) lui-même sa propre électricité de fonctionnement." };
+    public static readonly string[] CHAUSSURES = {"Chaussures", "Chaussures spécialement faites pour les mineurs. Elles sont renforcées pour protéger les pieds des mineurs." };
+    public static readonly string[] BRIQUET = { "Briquet","Outil qui permet de générer une petite flamme pour allumer une cigarette ou pour permettre au pénitant d'allumer une mèche. Cette mèche est située au bout d'une longue perche qui permet de faire exploser le grisou."};
+    public static readonly string[] LAMPISTERIE = {"Lampisterie", "Atelier, local où l'on range, comptabilise, entretient et répare les lampes portatives et les lanternes (dans une collectivité, une entreprise, une mine)." };
+    public static readonly string[] PELLE = {"Pelle", "Outil qui permettait aux mineurs de charger le charbon dans les berlines." };
+    public static readonly string[] PIOCHE = {"Pioche", "Utilisé par le « piqueur » il joue un grand rôle, car il permet de piocher dans la mine pour en prendre le charbon." };
+    public static readonly string[] CASQUE = {"Casque", "Dans les années cinquante, les casques en résine époxy ou en plastique ont remplacé les anciennes barrettes. Ces casques épais offraient une meilleure protection et étaient équipés pour recevoir la lampe électrique « au chapeau »." };
+    public static readonly string[] BIDON1 = {"Bidon 1", "Outil permettant aux mineurs de transporter des liquides." };
+    public static readonly string[] BIDON2 = {"Bidon 2", "Outil permettant aux mineurs de transporter des liquides." };
+    public static readonly string[] BURETTE1 = {"Burette 1", "Petit flacon à goulot (spécialement : prolongé d'un tube étroit pour verser goutte à goutte)." };
+    public static readonly string[] BURETTE2 = {"Burette 2", "Petit flacon à goulot" };
+    public static readonly string[] BURIN = {"Burin", " Outil en forme de ciseau d’acier que l’on pousse à la main et qui sert à graver, à couper les métaux. L’outillage du mineur (burin, coin, pointerolle, pic, masse) devait être entretenu quotidiennement pour conserver son efficacité." };
+    public static readonly string[] CLE = {"Clé", "Outil très classique utilisé même par les mineurs." };
+    public static readonly string[] GOURDE = {"Gourde", "Permet au mineur de boire en service pour se rafraichir." };
+    public static readonly string[] LAMPE = {"Lampe", "Meilleur ami du mineur puisqu'il lui permet tout simplement de voir !" };
+    public static readonly string[] POT = {"Pot", "Outil qui permettait aux mineurs de transporter avec eux un liquide." };
+
+    
    
 }
--- Assets/Scripts/UI/openmenuonclick.cs
+++ Assets/Scripts/UI/openmenuonclick.cs
@@ -8,8 +8,13 @@ public class openmenuonclick : MonoBehaviour
     public Text titre;
     public Text description;
 
+    // Méthode qui permet d'afficher une description de l'objet qui est en cours de visualisation. Elle est lancée quand
+    // quand un objet apparaît.
     public void openMenu()
     {
+        //La méthode switch va vérifier le nom de l'objet affiché et en fonction de ce nom, il va entrer dans la case correspondante
+        //Une fois dans la case, le programme choisit le bon titre et la bonne description depuis la classe ObjectsConstants
+        //Le break permet ensuite de sortir du switch
         switch(ModifiedTrackableEventHandler.lastimagename)
         {
             case "Image Briquet":
@@ -31,6 +36,58 @@ public class openmenuonclick : MonoBehaviour
             case "Image Lampisterie":
                 titre.text = ObjectsConstants.LAMPISTERIE[0];
                 description.text = ObjectsConstants.LAMPISTERIE[1];
+                break;
+            case "Image Pelle":
+                titre.text = ObjectsConstants.PELLE[0];
+                description.text = ObjectsConstants.PELLE[1];
+                break;
+            case "Image Pioche":
+                titre.text = ObjectsConstants.PIOCHE[0];
+                description.text = ObjectsConstants.PIOCHE[1];
+                break;
+            case "Image Casque":
+                titre.text = ObjectsConstants.CASQUE[0];
+                description.text = ObjectsConstants.CASQUE[1];
+                break;
+            case "Image Bidon1":
+                titre.text = ObjectsConstants.BIDON1[0];
+                description.text = ObjectsConstants.BIDON1[1];
+                break;
+            case "Image Bidon2":
+                titre.text = ObjectsConstants.BIDON2[0];
+                description.text = ObjectsConstants.BIDON2[1];
+                break;
+            case "Image Burette1":
+                titre.text = ObjectsConstants.BURETTE1[0];
+                description.text = ObjectsConstants.BURETTE1[1];
+                break;
+            case "Image Burette2":
+                titre.text = ObjectsConstants.BURETTE2[0];
+                description.text = ObjectsConstants.BURETTE2[1];
+                break;
+            case "Image Burin":
+                titre.text = ObjectsConstants.BURIN[0];
+                description.text = ObjectsConstants.BURIN[1];
+                break;
+            case "Image Cle":
+                titre.text = ObjectsConstants.CLE[0];
+                description.text = ObjectsConstants.CLE[1];
+                break;
+            case "Image Gourde":
+                titre.text = ObjectsConstants.GOURDE[0];
+                description.text = ObjectsConstants.GOURDE[1];
+                break;
+            case "Image Lampe":
+                titre.text = ObjectsConstants.LAMPE[0];
+                description.text = ObjectsConstants.LAMPE[1];
+                break;
+            case "Image Pot":
+                titre.text = ObjectsConstants.POT[0];
+                description.text = ObjectsConstants.POT[1];
+                break;
+            default :
+                titre.text = "Erreur";
+                description.text = "Objet introuvable.";
                 break;
         }
     }
