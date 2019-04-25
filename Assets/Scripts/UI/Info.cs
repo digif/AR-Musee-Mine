9b2a915d41def066cf9792b12dc868ec8260728c
digif <digifi@laposte.net>
2019-04-25 13:48:12 +0200

    only one file needed to add item descriptions, changed main menu title

---
+++ Assets/Scripts/UI/Info.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// structure to store the description of the objects
+/// </summary>
+public struct Info
+{
+    public readonly string objectName;
+    public readonly string title;
+    public readonly string description;
+
+    public Info(string name, string title, string desc)
+    {
+        objectName = name;
+        this.title = title;
+        description = desc;
+    }
+}
+
--- Assets/Scripts/UI/OpenMenuOnClick.cs
+++ Assets/Scripts/UI/OpenMenuOnClick.cs
@@ -8,91 +8,36 @@ public class OpenMenuOnClick : MonoBehaviour
     public Text titre;
     public Text description;
 
-    // Méthode qui permet d'afficher une description de l'objet qui est en cours de visualisation. Elle est lancée quand
-    // quand un objet apparaît.
+    private void Start()
+    {
+        ObjectsConstants.Initialize();
+    }
+
+    // put the description for the object currently looked at when clicking on the info button
     public void OpenMenu()
     {
-        //La méthode switch va vérifier le nom de l'objet affiché et en fonction de ce nom, il va entrer dans la case correspondante
-        //Une fois dans la case, le programme choisit le bon titre et la bonne description depuis la classe ObjectsConstants
-        //Le break permet ensuite de sortir du switch
-        switch(ModifiedTrackableEventHandler.lastimagename)
+        bool found = false;
+
+        // get the name of the object currently on screen 
+        string currentObject = ModifiedTrackableEventHandler.lastimagename;
+
+        //check that name against all the descriptions, asign the title and description if a match is found
+        foreach ( Info element in ObjectsConstants.infos)
         {
-            case "Image Briquet":
-                titre.text = ObjectsConstants.BRIQUET[0];
-                description.text = ObjectsConstants.BRIQUET[1];
-                break;
-            case "Image Grisoumetre":
-                titre.text = ObjectsConstants.GRISOUMETRE[0];
-                description.text = ObjectsConstants.GRISOUMETRE[1];
-                break;
-            case "Image Genephone":
-                titre.text = ObjectsConstants.GENEPHONE[0];
-                description.text = ObjectsConstants.GENEPHONE[1];
-                break;
-            case "Image Chaussures":
-                titre.text = ObjectsConstants.CHAUSSURES[0];
-                description.text = ObjectsConstants.CHAUSSURES[1];
-                break;
-            case "Image Lampisterie":
-                titre.text = ObjectsConstants.LAMPISTERIE[0];
-                description.text = ObjectsConstants.LAMPISTERIE[1];
-                break;
-            case "Image Pelle":
-                titre.text = ObjectsConstants.PELLE[0];
-                description.text = ObjectsConstants.PELLE[1];
-                break;
-            case "Image Pioche":
-                titre.text = ObjectsConstants.PIOCHE[0];
-                description.text = ObjectsConstants.PIOCHE[1];
-                break;
-            case "Image Casque":
-                titre.text = ObjectsConstants.CASQUE[0];
-                description.text = ObjectsConstants.CASQUE[1];
-                break;
-            case "Image Bidon1":
-                titre.text = ObjectsConstants.BIDON1[0];
-                description.text = ObjectsConstants.BIDON1[1];
-                break;
-            case "Image Bidon2":
-                titre.text = ObjectsConstants.BIDON2[0];
-                description.text = ObjectsConstants.BIDON2[1];
-                break;
-            case "Image Burette1":
-                titre.text = ObjectsConstants.BURETTE1[0];
-                description.text = ObjectsConstants.BURETTE1[1];
-                break;
-            case "Image Burette2":
-                titre.text = ObjectsConstants.BURETTE2[0];
-                description.text = ObjectsConstants.BURETTE2[1];
-                break;
-            case "Image Burin":
-                titre.text = ObjectsConstants.BURIN[0];
-                description.text = ObjectsConstants.BURIN[1];
-                break;
-            case "Image Cle":
-                titre.text = ObjectsConstants.CLE[0];
-                description.text = ObjectsConstants.CLE[1];
-                break;
-            case "Image Gourde":
-                titre.text = ObjectsConstants.GOURDE[0];
-                description.text = ObjectsConstants.GOURDE[1];
-                break;
-            case "Image Lampe":
-                titre.text = ObjectsConstants.LAMPE[0];
-                description.text = ObjectsConstants.LAMPE[1];
-                break;
-            case "Image Pot":
-                titre.text = ObjectsConstants.POT[0];
-                description.text = ObjectsConstants.POT[1];
-                break;
-            case "Image Hache":
-                titre.text = ObjectsConstants.HACHE[0];
-                description.text = ObjectsConstants.HACHE[1];
-                break;
-            default :
-                titre.text = "Erreur";
-                description.text = "Objet introuvable.";
-                break;
+            if (currentObject == element.objectName)
+            {
+                titre.text = element.title;
+                description.text = element.description;
+                found = true;
+            }
+        }
+
+        // if a match isn't found, fill the info menu with an error message instead
+        // (this message isn't accessible in normal use as the info button only appear if an item is currently active)
+        if(found == false)
+        {
+            titre.text = "Erreur";
+            description.text = "Objet introuvable.";
         }
     }
 }
